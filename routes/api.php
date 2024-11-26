104e07f21f054ca8a4616f0dfeed695e329999dd
Mangel111111111 <mangel111111111@gmail.com>
2024-11-26 15:47:10 +0100

    feat: add api controllers base

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\VacancyController;
+use App\Http\Controllers\Api\VacancyController;
 
 Route::get('/', [VacancyController::class,'index'])->name('apiIndex');
