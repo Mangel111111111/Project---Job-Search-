bf5b3a8fc125244f93f2a0a85102da18421b8652
Mangel111111111 <mangel111111111@gmail.com>
2024-11-28 12:06:24 +0100

    test: improve test coverage

--- app/Http/Controllers/Api/VacancyController.php
+++ app/Http/Controllers/Api/VacancyController.php
@@ -20,10 +20,10 @@ class VacancyController extends Controller
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
+    /* public function create()
     {
         //
-    }
+    } */
 
     /**
      * Store a newly created resource in storage.
@@ -41,18 +41,18 @@ class VacancyController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    /* public function show(string $id)
     {
         //
-    }
+    } */
 
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(string $id)
+    /* public function edit(string $id)
     {
-        //
-    }
+        
+    } */
 
     /**
      * Update the specified resource in storage.
