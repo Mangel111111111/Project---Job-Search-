9ef8b1f77cb682b6ab07c673ab44b758bb3e2bad
Mangel111111111 <mangel111111111@gmail.com>
2024-12-10 15:04:27 +0100

    feat: add footer and footer style

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,4 +18,19 @@
     </div>
     <script src="{{ asset('js/app.js') }}"></script>
 </body>
+<footer>
+    <table>
+        <tr>
+            <a href="https://www.linkedin.com/in/miguel-garcía-lópez-609136284" target="_blank">
+                <img src="https://res.cloudinary.com/del1j3jge/image/upload/v1733839201/csrdiruuofgnk4zulpdq.webp" alt="linkedIn">
+            </a>
+            <a href="https://github.com/Mangel111111111" target="_blank">
+                <img src="https://res.cloudinary.com/del1j3jge/image/upload/v1733839332/rwotr7xaslcpz3dv5vkg.png" alt="github">
+            </a>
+        </tr>
+        <tr>
+            <h2>Made by: Miguel Ángel García López</h2>
+        </tr>
+    </table>
+</footer>
 </html>
