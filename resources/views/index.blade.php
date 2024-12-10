c2e4610cc72f83e6e3c4f97ec55dbc6625f7a28c
Mangel111111111 <mangel111111111@gmail.com>
2024-12-10 19:21:27 +0100

    feat: add body desing and show

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,8 +8,8 @@
                     <th scope="col">ID</th>
                     <th scope="col">Date Apply</th>
                     <th scope="col">Last Update</th>
-                    <th scope="col">Company</th>
                     <th scope="col">Work Apply</th>
+                    <th scope="col">Status</th>
                     <th scope="col">News</th>
                 </tr>
             </thead>
@@ -19,8 +19,8 @@
                         <td>{{ $vacancy->id }}</td>
                         <td>{{ $vacancy->created_at->format("d/m/y") }}</td>
                         <td>{{ $vacancy->updated_at->format("d/m/Y") }}</td>
-                        <td>{{ $vacancy->company }}</td>
-                        <td>{{ $vacancy->workapply }}</td>
+                        <td><a href="http://127.0.0.1:8000/vacancies/{{$vacancy->id}}">{{ $vacancy->jobOffer }}</a></td>
+                        <td>{{ $vacancy->jobVacancyStatus }}</td>
                         <td>
                             <ul>
                                 @forelse ($vacancy->follows as $follow)
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -1,5 +1,19 @@
+
 @extends('layouts.app')
 
 @section('content')
-        {$item->id}
+    <div class="vacancy-details">
+        <h1>{{ $vacancy->jobOffer }}</h1>
+        <p><strong>ID:</strong> {{ $vacancy->id }}</p>
+        <p><strong>Date Apply:</strong> {{ $vacancy->created_at->format("d/m/y") }}</p>
+        <p><strong>Last Update:</strong> {{ $vacancy->updated_at->format("d/m/Y") }}</p>
+        <p><strong>Status:</strong> {{ $vacancy->jobVacancyStatus }}</p>
+        
+        <h3>News:</h3>
+        <ul>
+            @foreach ($vacancy->follows as $follow)
+                <li>{{ $follow->id }} - {{ $follow->news }}</li>
+            @endforeach
+        </ul>
+    </div>
 @endsection
