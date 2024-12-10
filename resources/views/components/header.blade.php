7a1673eaad62c0bcd77e65bb0b4ffc4cb3cdc55c
Mangel111111111 <mangel111111111@gmail.com>
2024-12-10 15:31:43 +0100

    feat: add header and header style

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -1,3 +1,9 @@
 <header>
-    <h1>rgwerfrfwfef</h1>
+    <header>
+        <h1>Project Job Search
+        </h1>
+    </header>
+    <nav>
+        <a href="http://127.0.0.1:8000/">Main Page</a>
+    </nav>
 </header>
